c60b168997612a81af56c014be4a9b276f25a83a
surren01522-tech <surren01522@gmail.com>
2026-05-20 15:38:13 +0900

    11강 ing

---
+++ 2026_TopDown/Assets/Scripts/GameDataManager.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[Serializable]
+public class PlayerData
+{
+    public List<string> collectedItems = new List<string>();
+    public int Stage = 1;
+}
+
+public class GameDataManager : MonoBehaviour
+{
+    public static GameDataManager Instance;
+    public PlayerData playerData;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void SaveData(PlayerData playerData)
+    {
+        string filePath = Application.persistentDataPath + "/player_data.json";
+        string json = JsonUtility.ToJson(playerData, true);
+        System.IO.File.WriteAllText(filePath, json);
+        Debug.Log("게임 데이터 저장됨: " + json);
+    }
+
+    public PlayerData LoadData()
+    {
+        string filePath = Application.persistentDataPath + "/player_data.json";
+        if (System.IO.File.Exists(filePath))
+        {
+            string json = System.IO.File.ReadAllText(filePath);
+            PlayerData playerData = JsonUtility.FromJson<PlayerData>(json);
+            Debug.Log("게임 데이터 로드됨: " + json);
+            return playerData;
+        }
+        else
+        {
+            Debug.LogWarning("저장된 게임 데이터가 없습니다.");
+            return new PlayerData();
+        }
+    }
+
+
+}
